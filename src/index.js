1c77a0fd6ff8b09872e3d82122e46a7544918bb2
JSdev-coder <66922979+JSdevS-coder@users.noreply.github.com>
2022-07-04 13:50:15 +0200

    fixing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
->>>>>>> parent of 4d2042e (adding navbar and hero section)
--- src/redux/user.js
+++ src/redux/user.js
@@ -2,32 +2,29 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
-  data: {},
-  isLoaded: false,
-  hasErrors: false,
-};
+	data: {},
+	isLoaded: false,
+	hasErrors: false,
+}
 
 const user = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    getData: (state) => {
-    },
+	name: 'user',
+	initialState,
+	reducers: {
+		getData: state => {},
 
-    getDataSuccess: (state, action) => {
-      state.isLoaded = true;
-      state.data = action.payload;
-    },
+		getDataSuccess: (state, action) => {
+			state.isLoaded = true
+			state.data = action.payload
+		},
 
-    getDataFailure: (state) => {
-      state.isLoaded = true;
-      state.hasErrors = true;
-    },
-  }
-});
+		getDataFailure: state => {
+			state.isLoaded = true
+			state.hasErrors = true
+		},
+	},
+})
 
-export const reducer = user.reducer;
+export const reducer = user.reducer
 
-export const {
-  getData, getDataSuccess, getDataFailure
-} = user.actions;
+export const { getData, getDataSuccess, getDataFailure } = user.actions
